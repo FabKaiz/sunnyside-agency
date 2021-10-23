147720be8b359dd31ff19ee869ee64b3ce08963d
Fabien <cacrfabfr@gmail.com>
2021-10-23 10:50:07 +0200

    project setup

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="nav-container">
+    <div class="nav-left">
+      <router-link to="/">sunnyside</router-link>
+    </div>
+    <div class="nav-right">
+      <ul>
+        <li>About</li>
+        <li>Services</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="not-found">
+    <router-link to="/">Back to home</router-link>
+    <h1>404</h1>
+    <h2>The page you were looking for does not exist</h2>
+  </div>
+</template>
